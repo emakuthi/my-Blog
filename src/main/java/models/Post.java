d7e1017dde661bf58dde3a7941dc7085395dea55
emakuthi <emakuthi@gmail.com>
2019-05-22 10:20:32 +0300

    create blog app

---
+++ src/main/java/models/Post.java
@@ -0,0 +1,27 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Post {
+
+    private String content;
+    private static ArrayList<Post> instances = new ArrayList<>();// I’m new. When do I get created?
+
+    public Post(String content) {
+         this.content = content;
+         instances.add(this);//Also new. Can you figure out what I do and how I work?
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+
+    public static void clearAllPosts(){
+        instances.clear();
+    }
+
+    public static ArrayList<Post> getAll() {
+        return instances;
+    }
+}
---
+++ src/test/java/models/PostTest.java
@@ -0,0 +1,53 @@
+package models;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PostTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+//    @After
+//    public void tearDown() throws Exception {
+//
+//    }
+
+    @Test
+    public void NewPostObjectsCorrectlyCreated_true() throws Exception {
+
+        Post post = new Post ("Day 1: Intro");
+    }
+
+    @Test
+    public void PostInstantiatesWithContent_true() throws  Exception {
+
+        Post post = new Post ("Day 1: Intro");
+
+        assertEquals("Day 1: Intro" , post.getContent());
+    }
+    @After
+    public void tearDown() {
+        Post.clearAllPosts(); //clear out all the posts before each test.
+    }
+
+    @Test
+    public void AllPostsAreCorrectlyReturned_true() {
+        Post post = new Post("Day 1: Intro");
+        Post otherPost = new Post ("How to pair successfully");
+        assertEquals(2, Post.getAll().size());
+    }
+
+    @Test
+    public void AllPostsContainsAllPosts_true() {
+        Post post = new Post("Day 1: Intro");
+        Post otherPost = new Post ("How to pair successfully");
+        assertTrue(Post.getAll().contains(post));
+        assertTrue(Post.getAll().contains(otherPost));
+    }
+}
